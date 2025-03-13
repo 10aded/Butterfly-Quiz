2079224dba3d1067e7a35f5f4f5e20dd576cc47d
10aded <10aded-Streaming@protonmail.com>
2025-03-13 11:53:36 +1000

    made not of which line fails

--- main.zig
+++ main.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const embedded_photos = embed_photos();
 
 //const NUMBER = 72; // Builds fine.
-const NUMBER = 73; // Builds fine.
+const NUMBER = 73; // Gives weird compile error with no reference to this file.
 
 fn embed_photos() [NUMBER] [:0] const u8 {
     var result : [NUMBER] [:0] const u8 = undefined;
